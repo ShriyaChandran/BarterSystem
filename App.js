8cafe9d15a4b44fc0e532a4444b9a71150044d56
ShriyaChandran <62100109+ShriyaChandran@users.noreply.github.com>
2021-03-10 21:04:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import {SignupLoginScreen} from '.screens/SignupLoginScreen';
+
+export default function App (){
+  return(
+    <SignupLoginScreen/>
+  );
+}
---
+++ SignupLoginScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+
+export default class SignupLoginScreen extends Component{
+    render(){
+        return(
+            
+        )
+    }
+} 
