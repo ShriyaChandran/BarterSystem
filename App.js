bda3b0f993951547fc24222b23e5c56aa6646be1
ShriyaChandran <62100109+ShriyaChandran@users.noreply.github.com>
2021-05-03 11:45:56 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import * as React from 'react';
 import {View, Text, TextInput, StyleSheet} from 'react-native';
-import {SignupLoginScreen} from '.screens/SignupLoginScreen';
+import SignupLoginScreen from './screens/SignupLoginScreen';
+import {AppTabNavigator} from './components/AppTabNavigator';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 
-export default function App (){
-  return(
-    <SignupLoginScreen/>
-  );
+export default class App extends React.Component{
+  render(){
+    return(
+      <AppContainer/>
+    )
+  }
 }
+const switchNavigator = createSwitchNavigator({
+  SignupLoginScreen: {screen:SignupLoginScreen},
+  BottomTab:{screen:AppTabNavigator}
+})
+
+const AppContainer = createAppContainer(switchNavigator);
